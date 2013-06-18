9d6f1c957c3778c1982b3b8cfc6d1b29f24bbb0a
Viacheslav Bobrov <babrovka@gmail.com>
2013-06-18 17:49:17 +0200

    user photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :phone, :position, :organization, :division, :info, :access_level,
                   :email, :photo, :dob, :permit, :work_status
                               
-  WORK_STATUSES = [ 
-      [I18n.t('at_work'), 'at_work'],
-      [I18n.t('ooo'), 'ooo']
-    ]
+  WORK_STATUSES = [ [I18n.t('at_work'), 'at_work'], [I18n.t('ooo'), 'ooo'] ]
+  
+  has_attached_file :photo, :styles => { :thumb => "48x48#" }
+  
 end
