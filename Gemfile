1875991a50feeba366f26f23e744951dae7569b3
Viacheslav Bobrov <babrovka@gmail.com>
2013-06-18 08:59:27 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'capistrano'
+gem 'rvm-capistrano'
+gem 'execjs'
+gem 'therubyracer', '~> 0.10.2'
+
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails', '~> 2.3.0'
+
+group :production do
+  gem 'thin'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kzs::Application.config.secret_token = 'c187c5eec5c185a4696c9b453db01cee00c2f77259bd1e8a2dd294d2e9a59cebe5a1ce52b92f76c0fa468a1faa0be8af25970b07f5d76874770ffd270066092f'
