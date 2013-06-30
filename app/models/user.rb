164d17d44730c5d0e608f90a9a516e37f9d153ac
Viacheslav Bobrov <babrovka@gmail.com>
2013-06-30 19:05:46 +0200

    user image upload & form labels

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
-
   attr_accessible :name, :phone, :position, :organization_id, :division, :info,
                   :avatar, :dob, :permit, :work_status, :right_ids,
-                  :email, :password, :password_confirmation, :remember_me,
-                  :crop_x, :crop_y, :crop_w, :crop_h
+                  :email, :password, :password_confirmation, :remember_me
                   
   has_many :user_rights
   has_many :rights, through: :user_rights
@@ -23,27 +16,7 @@ class User < ActiveRecord::Base
                    :email, :dob, :permit, :work_status, :presence => true
   validates :phone, :length => { :maximum => 11 }
   
-  has_attached_file :avatar, :styles => { 
-    :small => {:geometry => "100x100#", :processors => [:cropper]}, 
-    :large => {:geometry => "500x500>"} 
-  }
-  
-  after_update :reprocess_avatar, :if => :cropping?
-  
-  def cropping?
-    !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
-  end
-  
-  def avatar_geometry(style = :original)
-    @geometry ||= {}
-    @geometry[style] ||= Paperclip::Geometry.from_file(avatar.path(style))
-  end
-  
-  private
-  
-  def reprocess_avatar
-      avatar.assign(avatar)
-      avatar.save
-    end
+  has_attached_file :avatar, :styles => { :small => "48x48#", :large => "100x100#" } 
+
   
 end
