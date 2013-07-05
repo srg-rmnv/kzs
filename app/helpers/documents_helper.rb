5a9ea79f7d4afac8b240a36c250e1d27eaeefb44
Viacheslav Bobrov <babrovka@gmail.com>
2013-07-05 13:11:37 +0200

    if clause for helper

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,11 +1,19 @@
 module DocumentsHelper
   
   def doc_user(user_id)
-    User.find(user_id).name
+    if User.exists?(user_id)
+      User.find(user_id).name
+    else
+      "Deleted"
+    end
   end
   
   def organization(organization_id)
-    Organization.find(organization_id).title
+    if Organization.exists?(organization_id)
+      Organization.find(organization_id).title
+    else
+      "Deleted"
+    end
   end   
   
 end
