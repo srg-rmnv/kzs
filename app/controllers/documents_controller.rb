246d46aa7f4912a32f331b498f183abbc1a082bb
Viacheslav Bobrov <babrovka@gmail.com>
2013-07-06 09:10:23 +0200

    disable sort link + redirects

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -64,7 +64,7 @@ class DocumentsController < ApplicationController
 
     respond_to do |format|
       if @document.save && 
-        format.html { redirect_to @document, notice: 'Document was successfully created.' }
+        format.html { redirect_to documents_path, notice: t('document_successfully_created') }
         format.json { render json: @document, status: :created, location: @document }
       else
         format.html { render action: "new" }
@@ -86,7 +86,7 @@ class DocumentsController < ApplicationController
 
     respond_to do |format|
       if @document.update_attributes(params[:document])
-        format.html { redirect_to @document, notice: 'Document was successfully updated.' }
+        format.html { redirect_to documents_path, notice: t('document_successfully_updated') }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
