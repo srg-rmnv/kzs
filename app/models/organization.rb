03a8045212139f34f00ca598378b5eb3f8e2086e
Viacheslav Bobrov <babrovka@gmail.com>
2013-09-20 12:35:58 +0400

    organization validation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,5 +2,7 @@ class Organization < ActiveRecord::Base
   attr_accessible :title, :parent_id, :lft, :rgt, :logo,
                   :address, :phone, :mail, :director_id
   acts_as_nested_set
+  
+  validates :title, :logo, :address, :phone, :mail, :director_id, :presence => true
   has_attached_file :logo, :styles => { :pdf => "120x70#" } 
 end
