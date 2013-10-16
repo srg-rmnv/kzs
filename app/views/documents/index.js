40c5e365df5d5fcd092cc2f83c4527397488769f
Viacheslav Bobrov <babrovka@gmail.com>
2013-10-16 16:51:56 +0400

    index.js fix

--- app/views/documents/index.js
+++ app/views/documents/index.js
@@ -1,1 +1,9 @@
 $('#document_table').html("<%=j render :partial => 'table', :@documents => @documents %>");
+$("tr").click(function() {
+	// $(this).hide();
+	document_id = $(this).find('.document_id').html()
+	$.ajax({
+	    url: "/documents/" + document_id,
+	    dataType: 'json',
+	  })	
+});
